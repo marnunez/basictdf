e239830f6e005a10bf95f82c6ef846739b98c420
Marcos Alfredo Núñez <mnunez@fleni.org.ar>
2022-05-12 13:00:57 -0300

    Type hints and docs, renamed the Type class

--- src/basictdf/tdfTypes.py
+++ src/basictdf/tdfTypes.py
@@ -2,7 +2,18 @@ __doc__ = "Shared classes and types for basictdf."
 
 from datetime import datetime
 import struct
+from typing import (
+    IO,
+    Any,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    BinaryIO,
+    Generic,
+)
 import numpy as np
+import numpy.typing as npt
 
 
 class BTSDate:
@@ -29,16 +40,26 @@ class BTSString:
     """
 
     @staticmethod
-    def read(size, data):
+    def read(size: int, data: bytes, encoding: str = "windows-1252") -> str:
+        """read a BTSString from bytes
+
+        Args:
+            size (int): size of the string to read
+            data (bytes): input bytes
+            encoding (str, optional): encoding to use. Defaults to "windows-1252".
+
+        Returns:
+            str: a Python string
+        """
         la = struct.unpack(f"{size}s", data)[0]
         try:
             pos = la.index(b"\x00")
-            return la[:pos].decode("windows-1252")
+            return la[:pos].decode(encoding)
         except ValueError:
-            return la.decode("windows-1252")
+            return la.decode(encoding)
 
     @staticmethod
-    def write(size, data):
+    def write(size: int, data: str) -> bytes:
         dat = data.encode("windows-1252") + b"\x00"
         padding = b"\x00" * (size - len(dat))
         if len(dat) > size:
@@ -46,55 +67,90 @@ class BTSString:
         return dat + padding
 
     @staticmethod
-    def bwrite(file, size, data):
+    def bwrite(file: BinaryIO, size: int, data: str):
         file.write(BTSString.write(size, data))
 
     @staticmethod
-    def bread(f, size):
-        return BTSString.read(size, f.read(size))
+    def bread(file: BinaryIO, size: int, encoding: str = "windows-1252") -> str:
+        """Read a BTSString from a binary file or buffer
+
+        Args:
+            file (BinaryIO): input binary file or buffer
+            size (int): size of the string to read
+            encoding (str, optional): encoding to use. Defaults to "windows-1252".
+
+        Returns:
+            str: a Python string
+        """
+        return BTSString.read(size, file.read(size), encoding=encoding)
 
 
-class Type:
-    def __init__(self, btype):
-        self.btype = btype
+# T = NewType("T", np.dtype)
+# T = TypeAlias(npt.DTypeLike)
+# T = TypeVar("T", bound=npt.DTypeLike)
+X = TypeVar("X", bound=np.dtype)
 
-    def read(self, data, n=-1):
-        if n == -1:
-            return np.frombuffer(data, dtype=self.btype)[0]
+
+class TdfType(Generic[X]):
+    def __init__(self, btype: npt.DTypeLike):
+        self.btype: X = np.dtype(btype)
+
+    def read(self, data: bytes) -> npt.NDArray[X]:
+        """Read data to the type
+
+        Args:
+            data (bytes): input bytes
+
+        Returns:
+            np.ndarray: output array with items of the requiered type
+        """
+        return np.frombuffer(data, dtype=self.btype)
+
+    def bread(
+        self, file: IO[bytes], n: Optional[int] = None
+    ) -> Union[npt.NDArray[X], X]:
+        """Read data from binary file or buffer
+
+        Args:
+            file (IO[Any]): input file or buffer
+            n (int, optional): Ammount of items to take. If _None_, returns a single item, otherwise returns an array of _n_ items. Defaults to None.
+
+        Returns:
+            Union[np.ndarray,type]: A numpy type (custom or classic, like numpy.float32) or a np.array of numpy types
+        """
+        if n is None:
+            return self.read(file.read(self.btype.itemsize))[0]
         else:
-            return np.frombuffer(data, dtype=self.btype)
+            return self.read(file.read(n * self.btype.itemsize))
 
-    def bread(self, file, n=-1):
-        return self.read(file.read(abs(n) * self.btype.itemsize), n=n)
-
-    def write(self, data):
+    def write(self, data: Union[npt.NDArray[X], X]):
         return (
             data.astype(self.btype.base).tobytes()
             if isinstance(data, np.ndarray)
             else np.array(data, dtype=self.btype.base).tobytes()
         )
 
-    def bwrite(self, file, data):
+    def bwrite(self, file: IO[bytes], data: Union[npt.NDArray[X], X]):
         file.write(self.write(data))
 
-    def skip(self, file, n=1):
+    def skip(self, file: IO[bytes], n: int = 1):
         file.seek(n * self.btype.itemsize, 1)
 
-    def pad(self, n=1):
+    def pad(self, n: int = 1):
         return b"\x00" * (n * self.btype.itemsize)
 
-    def bpad(self, file, n=1):
+    def bpad(self, file: IO[bytes], n: int = 1):
         file.write(self.pad(n))
 
 
-Volume = Type(np.dtype("3<f4"))
+Volume = TdfType(np.dtype("3<f4"))
 
-VEC3F = Type(np.dtype("3<f4"))
+VEC3F = TdfType(np.dtype("3<f4"))
 
-Matrix = Type(np.dtype("(3,3)<f4"))
+Matrix = TdfType(np.dtype("(3,3)<f4"))
 
 
-Int32 = Type(np.dtype("<i4"))
-Int16 = Type(np.dtype("<i2"))
-Uint32 = Type(np.dtype("<u4"))
-Float32 = Type(np.dtype("<f4"))
+Int32 = TdfType(np.dtype("<i4"))
+Int16 = TdfType(np.dtype("<i2"))
+Uint32 = TdfType(np.dtype("<u4"))
+Float32 = TdfType(np.dtype("<f4"))
