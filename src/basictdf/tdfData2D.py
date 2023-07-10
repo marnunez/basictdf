6c7c8272b44eb706f75d94221a2c11390f706f9b
Marcos A. Núñez <silver94@gmail.com>
2023-07-10 12:16:18 -0300

    Reworked data2D

--- src/basictdf/tdfData2D.py
+++ src/basictdf/tdfData2D.py
@@ -23,6 +23,10 @@ class Data2DFlags(Enum):
 
 
 class Data2DPCK:
+    def __init__(self, data) -> None:
+        self.data = data
+
+    @staticmethod
     def _build(stream, nFrames, nCameras):
         nPointsCaptured = u16.bread(stream, nCameras * nFrames).reshape(
             [nCameras, nFrames]
@@ -34,20 +38,35 @@ class Data2DPCK:
                 nPoints = nPointsCaptured[camera, frame]
                 if nPoints > 0:
                     data[frame, camera] = VEC2F.bread(stream, nPoints)
-        return data
+        return Data2DPCK(data)
 
-    def _write(stream, data) -> None:
-        nFrames, nCameras = data.shape
+    def _write(self, stream) -> None:
+        nFrames, nCameras = self.data.shape
         nPointsCaptured = np.zeros((nCameras, nFrames), dtype=np.uint16)
         for frame in range(nFrames):
             for camera in range(nCameras):
-                if data[frame, camera] is not None:
-                    nPointsCaptured[camera, frame] = len(data[frame, camera])
+                if self.data[frame, camera] is not None:
+                    nPointsCaptured[camera, frame] = len(self.data[frame, camera])
         u16.bwrite(stream, nPointsCaptured.flatten())
         for frame in range(nFrames):
             for camera in range(nCameras):
-                if data[frame, camera] is not None:
-                    VEC2F.bwrite(stream, data[frame, camera])
+                if self.data[frame, camera] is not None:
+                    VEC2F.bwrite(stream, self.data[frame, camera])
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Data2DPCK):
+            return False
+        return np.allclose(self.data, o.data)
+
+    @property
+    def nBytes(self):
+        nFrames, nCameras = self.data.shape
+        return 2 * nCameras * nFrames + sum(
+            self.data[i, j].nbytes
+            for i in range(nFrames)
+            for j in range(nCameras)
+            if self.data[i, j] is not None
+        )
 
 
 class Data2DBlockFormat(Enum):
@@ -71,7 +90,6 @@ class Data2D(Block):
         frequency: int,
         startTime: datetime,
         flags: Data2DFlags,
-        data: np.ndarray,
         format: Data2DBlockFormat = Data2DBlockFormat.PCKFormat,
     ) -> None:
         super().__init__()
@@ -84,7 +102,6 @@ class Data2D(Block):
         self.startTime = startTime
         "start time of the capture"
         self.flags: Data2DFlags = flags
-        self.data = data
         """a numpy array of shape (nFrames, nCams).
         Each element is a numpy array of shape (nPoints, 2) containing
         the 2D coordinates of the points captured by the camera at
@@ -93,6 +110,15 @@ class Data2D(Block):
         self.format = format
 
         self._camMap = []
+        self._data = None
+
+    @property
+    def data(self):
+        return self._data.data
+
+    @data.setter
+    def data(self, value):
+        self._data = Data2DPCK(value)
 
     @staticmethod
     def _build(stream, format) -> "Data2D":
@@ -107,8 +133,9 @@ class Data2D(Block):
         camMap = u16.bread(stream, nCams)
         data = Data2DPCK._build(stream, nFrames, nCams)
 
-        block = Data2D(nCams, nFrames, frequency, startTime, flags, data, format)
+        block = Data2D(nCams, nFrames, frequency, startTime, flags, format)
         block._camMap = camMap
+        block._data = data
 
         return block
 
@@ -123,23 +150,42 @@ class Data2D(Block):
         BTSDate.bwrite(stream, self.startTime)
         u32.bwrite(stream, self.flags.value)
         i16.bwrite(stream, self._camMap)
-        Data2DPCK._write(stream, self.data)
+        self._data._write(stream)
 
     @property
     def nBytes(self):
         return (
-            i32.btype.itemsize
-            + i32.btype.itemsize
-            + i32.btype.itemsize
+            4  # nCams
+            + 4  # nFrames
+            + 4  # frequency
             + 4  # BTSDate.nBytes
-            + u32.btype.itemsize
-            + i16.btype.itemsize * self.nCams
-            + self.data.nbytes
+            + 4  # flags
+            + 2 * self.nCams  # camMap
+            # Size of the data without the None elements
+            + self._data.nBytes
         )
 
     def __repr__(self) -> str:
         return (
-            f"<Data2D format={self.format.name} nCams={self.nCams}"
+            f"<Data2D format={self.format.name} nCams={self.nCams} "
             f"nFrames={self.nFrames} frequency={self.frequency} "
             f"startTime={self.startTime} flags={self.flags.name} >"
+        )
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Data2D):
+            return False
+        return (
+            self.nCams == o.nCams
+            and self.nFrames == o.nFrames
+            and self.frequency == o.frequency
+            and self.startTime == o.startTime
+            and self.flags == o.flags
+            and all(
+                np.array_equal(self.data[i, j], o.data[i, j])
+                for i in range(self.nFrames)
+                for j in range(self.nCams)
+            )
+            and self.format == o.format
+            and self.nBytes == o.nBytes
         )
